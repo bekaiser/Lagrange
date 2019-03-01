dfa583ea708dff249196673392f750277a7d2df6
bekaiser <bkaiser@mit.edu>
2019-02-28 17:38:08 -0700

    added Neumann lower BC

--- differences.py
+++ differences.py
@@ -28,9 +28,10 @@ def weights2( z0 , z1 , z2 , z3 , zj ):
  return l0, l1, l2, l3
 
 
-def partial_zz( z ):
+def partial_zz( z , lower_BC_flag , upper_BC_flag ):
  # second derivative, permiting non-uniform grids
  Nz = np.shape(z)[0]
+
  # 2nd order accurate (truncated 3rd order terms), variable grid
  diagm1 = np.zeros([Nz-1])
  diag0 = np.zeros([Nz])
@@ -41,20 +42,28 @@ def partial_zz( z ):
      diagp1[j] =   ( z[j] - z[j-1] ) / denom
      diag0[j] =  - ( z[j+1] - z[j-1] ) / denom
  pzz = np.diag(diagp1,k=1) + np.diag(diagm1,k=-1) + np.diag(diag0,k=0) 
+
  # lower (wall) BC sets variable to zero at the wall
  zj = z[0] # location of derivative for lower BC (first cell center)
  l0, l1, l2, l3 = weights2( -z[0] , z[0] , z[1] , z[2] , zj )
- l1 = l1 - l0 # Dirchlet phi=0 at z=0 (sets phi_ghost = -phi_1)
+ if lower_BC_flag == 'dirchlet':
+   l1 = l1 - l0 # Dirchlet phi=0 at z=0 (sets phi_ghost = -phi_0)
+ if lower_BC_flag == 'neumann':
+   l1 = l1 + l0 # Neumann for dz(phi)=0 at z=0 (sets phi_ghost = phi_0)
  pzz[0,0:3] = [ l1 , l2 , l3 ]
+
  # upper (far field) BC
  zj = z[Nz-1] # location of derivative for upper BC
  l0, l1, l2, l3 = weights2( z[Nz-3] , z[Nz-2] , z[Nz-1] , H + (H-z[Nz-1]) , zj )
- l2 = l3 + l2 # Neumann for phi_z=0 at z=H
+ if upper_BC_flag == 'dirchlet':
+   l2 = l2 - l3 # Dirchlet phi=0 at z=H (sets phi_ghost = -phi_N)
+ if upper_BC_flag == 'neumann':
+   l2 = l3 + l2 # Neumann for dz(phi)=0 at z=H (sets phi_ghost = phi_N)
  pzz[Nz-1,Nz-3:Nz] = [ l0 , l1 , l2 ]
  return pzz
 
 
-def partial_z( z ):
+def partial_z( z , lower_BC_flag , upper_BC_flag ):
  # first-order derivative matrix 
  # 2nd order accurate truncation
  Nz = np.shape(z)[0]
@@ -70,14 +79,20 @@ def partial_z( z ):
    diag0[j] =  ( ( z[j+1] - z[j] )**2. - ( z[j] - z[j-1] )**2. ) / denom
  pz = np.diag(diagp1,k=1) + np.diag(diagm1,k=-1) + np.diag(diag0,k=0) 
 
- # lower (wall) BC sets variable to zero at the wall
+ # lower BC
  l0, l1, l2, l3 = weights( -z[0] , z[0] , z[1] , z[2] , z[0] )
- l1 = l1 - l0 # Dirchlet phi=0 at z=0
+ if lower_BC_flag == 'dirchlet':
+   l1 = l1 - l0 # Dirchlet phi=0 at z=0 (sets phi_ghost = -phi_0)
+ if lower_BC_flag == 'neumann':
+   l1 = l1 + l0 # Neumann for dz(phi)=0 at z=0 (sets phi_ghost = phi_0)
  pz[0,0:3] = [ l1 , l2 , l3 ]
    
  # upper (far field) BC
  l0, l1, l2, l3 = weights( z[Nz-3] , z[Nz-2] , z[Nz-1] , H + (H-z[Nz-1]) , z[Nz-1] )
- l2 = l3 + l2 # Neumann for phi_z=0 at z=H
+ if upper_BC_flag == 'dirchlet':
+   l2 = l2 - l3 # Dirchlet phi=0 at z=H (sets phi_ghost = -phi_N)
+ if upper_BC_flag == 'neumann':
+   l2 = l3 + l2 # Neumann for dz(phi)=0 at z=H (sets phi_ghost = phi_N)
  pz[Nz-1,Nz-3:Nz] = [ l0 , l1 , l2 ]
  
  return pz
@@ -113,6 +128,11 @@ Linf1c = np.zeros([Ng]) # infinity norm, 1st derivative, cosine grid
 Linf2 = np.zeros([Ng]) # infinity norm, 2nd derivative, uniform grid 
 Linf2c = np.zeros([Ng]) # infinity norm, 2nd derivative, cosine grid 
 
+Linf12 = np.zeros([Ng]) # infinity norm, 1st derivative, uniform grid
+Linf1c2 = np.zeros([Ng]) # infinity norm, 1st derivative, cosine grid
+
+Linf22 = np.zeros([Ng]) # infinity norm, 2nd derivative, uniform grid 
+Linf2c2 = np.zeros([Ng]) # infinity norm, 2nd derivative, cosine grid 
  
 H = 1.0 # domain height
 
@@ -148,8 +168,8 @@ for n in range(0,Ng):
    plt.savefig(plotname,format="png"); plt.close(fig);
 
   U0 = 2. # free stream velocity
-
   m = np.pi/(2.*H)
+
   u = np.zeros([Nz,1])
   uz = np.zeros([Nz,1])
   uzz = np.zeros([Nz,1])
@@ -164,7 +184,42 @@ for n in range(0,Ng):
   uzc[:,0] = U0*m*np.cos(m*zc) 
   uzzc[:,0] = -U0*m**2.*np.sin(m*zc)
 
+  q = 2.*np.pi/(H)
+  b = np.zeros([Nz,1])
+  bz = np.zeros([Nz,1])
+  bzz = np.zeros([Nz,1])
+  b[:,0] = U0*np.cos(q*z)
+  bz[:,0] = -U0*q*np.sin(q*z) 
+  bzz[:,0] = -U0*q**2.*np.cos(q*z) 
+
+  bc = np.zeros([Nz,1])
+  bzc = np.zeros([Nz,1])
+  bzzc = np.zeros([Nz,1])
+  bc[:,0] = U0*np.cos(q*zc) 
+  bzc[:,0] = -U0*q*np.sin(q*zc) 
+  bzzc[:,0] = -U0*q**2.*np.cos(q*zc)
+
   if n == 6:
+   plotname = figure_path + 'solutions_uniform_grid_case2.png'
+   fig = plt.figure(figsize=(8,8))
+   plt.plot(b/U0,z,'b',label=r"$b$")
+   plt.plot(-bz/(q*U0),z,'k',label=r"$b_z$")
+   plt.plot(-bzz/(q**2.*U0),z,'--r',label=r"$b_{zz}$")
+   plt.ylabel(r"$z$",fontsize=13)
+   plt.title(r"normalized analytical solutions",fontsize=13)
+   plt.grid(); plt.legend(loc=3,fontsize=13)
+   plt.savefig(plotname,format="png"); plt.close(fig);
+
+   plotname = figure_path + 'solutions_cosine_grid_case2.png'
+   fig = plt.figure(figsize=(8,8))
+   plt.plot(bc/U0,zc,'b',label=r"$b$")
+   plt.plot(-bzc/(q*U0),zc,'k',label=r"$b_z$")
+   plt.plot(-bzzc/(q**2.*U0),zc,'--r',label=r"$b_{zz}$")
+   plt.ylabel(r"$z$",fontsize=13)
+   plt.title(r"normalized analytical solutions",fontsize=13)
+   plt.grid(); plt.legend(loc=3,fontsize=13)
+   plt.savefig(plotname,format="png"); plt.close(fig);
+
    plotname = figure_path + 'solutions_uniform_grid.png'
    fig = plt.figure(figsize=(8,8))
    plt.plot(u/U0,z,'b',label=r"$u$")
@@ -185,16 +240,21 @@ for n in range(0,Ng):
    plt.grid(); plt.legend(loc=6,fontsize=13)
    plt.savefig(plotname,format="png"); plt.close(fig);
 
-  #print( partial_z( z , 0 ) )
-  #print( partial_z( z , 1 ) )
-
   # compute 1st derivatives:
-  uz0 = np.dot( partial_z( z ) , u ) # uniform grid
-  uz0c = np.dot( partial_z( zc ) , uc ) # cosine grid
+  uz0 = np.dot( partial_z( z , 'dirchlet' , 'neumann' ) , u ) # uniform grid  
+  uz0c = np.dot( partial_z( zc , 'dirchlet' , 'neumann' ) , uc ) # cosine grid
 
   # compute 2nd dericatives:
-  uzz0 = np.dot( partial_zz( z ) , u ) # uniform grid
-  uzz0c = np.dot( partial_zz( zc ) , uc ) # cosine grid
+  uzz0 = np.dot( partial_zz( z , 'dirchlet' , 'neumann' ) , u ) # uniform grid
+  uzz0c = np.dot( partial_zz( zc , 'dirchlet' , 'neumann' ) , uc ) # cosine grid
+
+  # compute 1st derivatives:
+  bz0 = np.dot( partial_z( z , 'neumann' , 'neumann' ) , b ) # uniform grid  
+  bz0c = np.dot( partial_z( zc , 'neumann' , 'neumann' ) , bc ) # cosine grid
+
+  # compute 2nd dericatives:
+  bzz0 = np.dot( partial_zz( z , 'neumann' , 'neumann' ) , b ) # uniform grid
+  bzz0c = np.dot( partial_zz( zc , 'neumann' , 'neumann' ) , bc ) # cosine grid
 
   if n == 6:
    plotname = figure_path + 'computed_1st_derivative_uniform_grid.png'
@@ -268,11 +328,46 @@ for n in range(0,Ng):
    plt.grid(); plt.savefig(plotname,format="png"); plt.close(fig);
 
 
+   plotname = figure_path + 'computed_1st_derivative_uniform_grid_error_case2.png'
+   fig = plt.figure(figsize=(8,8))
+   plt.plot(abs(bz-bz0)/abs(q*U0),z,'k') 
+   plt.ylabel(r"$z$",fontsize=13)
+   plt.title(r"normalized 1$^{st}$ derivative, N =%i" %(Nz),fontsize=13)
+   plt.grid(); plt.savefig(plotname,format="png"); plt.close(fig);
+
+   plotname = figure_path + 'computed_1st_derivative_cosine_grid_error_case2.png'
+   fig = plt.figure(figsize=(8,8))
+   plt.plot(abs(bzc-bz0c)/abs(q*U0),z,'k') 
+   plt.ylabel(r"$z$",fontsize=13)
+   plt.title(r"normalized 1$^{st}$ derivative, N =%i" %(Nz),fontsize=13)
+   plt.grid(); plt.savefig(plotname,format="png"); plt.close(fig);
+
+   plotname = figure_path + 'computed_2nd_derivative_uniform_grid_error_case2.png'
+   fig = plt.figure(figsize=(8,8))
+   plt.plot(abs(bzz-bzz0)/abs(q**2.*U0),z,'k') 
+   plt.ylabel(r"$z$",fontsize=13)
+   plt.title(r"normalized 2$^{nd}$ derivative, N =%i" %(Nz),fontsize=13)
+   plt.grid(); plt.savefig(plotname,format="png"); plt.close(fig);
+
+   plotname = figure_path + 'computed_2nd_derivative_cosine_grid_error_case2.png'
+   fig = plt.figure(figsize=(8,8))
+   plt.plot(abs(bzzc-bzz0c)/abs(q**2.*U0),z,'k') 
+   plt.ylabel(r"$z$",fontsize=13)
+   plt.title(r"normalized 2$^{nd}$ derivative, N =%i" %(Nz),fontsize=13)
+   plt.grid(); plt.savefig(plotname,format="png"); plt.close(fig);
+
+
   Linf1[n] = np.amax(abs(uz-uz0)/abs(m*U0)) 
   Linf1c[n] = np.amax(abs(uzc-uz0c)/abs(m*U0))
 
   Linf2[n] = np.amax(abs(uzz-uzz0)/abs(m**2.*U0)) 
   Linf2c[n] = np.amax(abs(uzzc-uzz0c)/abs(m**2.*U0))
+
+  Linf12[n] = np.amax(abs(bz-bz0)/abs(q*U0)) 
+  Linf1c2[n] = np.amax(abs(bzc-bz0c)/abs(q*U0))
+
+  Linf22[n] = np.amax(abs(bzz-bzz0)/abs(q**2.*U0)) 
+  Linf2c2[n] = np.amax(abs(bzzc-bzz0c)/abs(q**2.*U0))
 
 
 plotname = figure_path + 'first_derivative_error.png'
@@ -289,6 +384,27 @@ plotname = figure_path + 'second_derivative_error.png'
 fig = plt.figure(figsize=(8,8))
 plt.loglog(Nr,Linf2,'r',label=r"uniform")
 plt.loglog(Nr,Linf2c,'b',label=r"cosine")
+plt.xlabel(r"$N$ grid points",fontsize=13)
+plt.ylabel(r"L$_\infty$ error",fontsize=13)
+plt.title(r"2$^{nd}$ derivative error",fontsize=13)
+plt.grid(); plt.legend(loc=1,fontsize=13)
+plt.savefig(plotname,format="png"); plt.close(fig);
+
+
+plotname = figure_path + 'first_derivative_error_case2.png'
+fig = plt.figure(figsize=(8,8))
+plt.loglog(Nr,Linf12,'r',label=r"uniform")
+plt.loglog(Nr,Linf1c2,'b',label=r"cosine")
+plt.xlabel(r"$N$ grid points",fontsize=13)
+plt.ylabel(r"L$_\infty$ error",fontsize=13)
+plt.title(r"1$^{st}$ derivative error",fontsize=13)
+plt.grid(); plt.legend(loc=1,fontsize=13)
+plt.savefig(plotname,format="png"); plt.close(fig);
+
+plotname = figure_path + 'second_derivative_error_case2.png'
+fig = plt.figure(figsize=(8,8))
+plt.loglog(Nr,Linf22,'r',label=r"uniform")
+plt.loglog(Nr,Linf2c2,'b',label=r"cosine")
 plt.xlabel(r"$N$ grid points",fontsize=13)
 plt.ylabel(r"L$_\infty$ error",fontsize=13)
 plt.title(r"2$^{nd}$ derivative error",fontsize=13)
